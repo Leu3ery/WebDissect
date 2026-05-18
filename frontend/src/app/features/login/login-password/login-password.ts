f21bd020d777f75cbea6910f372283695d5c8003
Valentyn <valentun.gvozdenko.04022008@gmail.com>
2026-05-18 08:26:48 +0200

    css + basic routes

---
+++ frontend/src/app/features/login/login-password/login-password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-password',
+  imports: [],
+  templateUrl: './login-password.html',
+  styleUrl: './login-password.css',
+})
+export class LoginPassword {
+
+}
