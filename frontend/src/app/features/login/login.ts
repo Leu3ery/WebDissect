2cda8fa980e9dabb4162e1dcb5a7c3d569641343
Valentyn <valentun.gvozdenko.04022008@gmail.com>
2026-05-22 13:02:01 +0200

    login page with switch button

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {LoginPassword} from './login-password/login-password';
+import {LoginEmail} from './login-email/login-email';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    LoginPassword,
+    LoginEmail
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  isLoginWithPassword = signal(false);
 
+  toggleLoginPage() {
+    this.isLoginWithPassword.set(!this.isLoginWithPassword())
+  }
 }
