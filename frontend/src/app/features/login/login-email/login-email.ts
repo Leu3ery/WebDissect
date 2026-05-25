65efbc7ca12d9b233696c8540c2ff115f4f891b2
Valentyn <valentun.gvozdenko.04022008@gmail.com>
2026-05-25 22:09:17 +0200

    login with email + login with password visual

--- frontend/src/app/features/login/login-email/login-email.ts
+++ frontend/src/app/features/login/login-email/login-email.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, QueryList, signal, ViewChild, ViewChildren} from '@angular/core';
+import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-email',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-email.html',
   styleUrl: './login-email.css',
 })
 export class LoginEmail {
+  @ViewChildren('digitInput') inputs!: QueryList<ElementRef>;
+  interval: undefined | number = undefined;
+  private fb = inject(FormBuilder);
+  stage = signal(1);
+  readyToResendIn = signal(5);
 
+  mailForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email, Validators.pattern("[a-z]+.[a-z]+@htlstp.at")]],
+  })
+
+  codeForm = this.fb.group({
+    digits: this.fb.array(
+      Array(6).fill('').map(() =>
+        this.fb.control('', [Validators.required, Validators.pattern('[0-9]')])
+      )
+    )
+  });
+
+  get digits() {
+    return this.codeForm.get('digits') as FormArray;
+  }
+
+  fullCode() {
+    return this.digits.value.join("");
+  }
+
+  goToStage(stage: number) {
+    this.stage.set(stage);
+    if (stage == 2) {
+      setTimeout(() => {
+        this.inputs.first?.nativeElement.focus();
+      })
+
+      this.readyToResendIn.set(5)
+
+      this.interval = setInterval(() => {
+        this.readyToResendIn.set(this.readyToResendIn() - 1);
+        if (this.readyToResendIn() == 0) {
+          clearInterval(this.interval);
+        }
+      }, 1000)
+    }
+  }
+
+  verify() {
+    if (this.codeForm.invalid) return;
+  }
+
+  sendCode() {
+    if (this.mailForm.invalid) return;
+    this.codeForm.reset();
+    this.goToStage(2)
+  }
+
+  onInput(e: Event, i: number) {
+    const val = (e.target as HTMLInputElement).value;
+    if (val && i < 5) {
+      this.inputs.toArray()[i + 1].nativeElement.focus();
+    }
+  }
+
+  onKeydown(e: KeyboardEvent, i: number) {
+    if (e.key === 'Backspace' && !this.digits.at(i).value && i > 0) {
+      this.inputs.toArray()[i - 1].nativeElement.focus();
+    }
+  }
 }
--- frontend/src/app/features/login/login-password/login-password.ts
+++ frontend/src/app/features/login/login-password/login-password.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login-password',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './login-password.html',
   styleUrl: './login-password.css',
 })
 export class LoginPassword {
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email, Validators.pattern("[a-z]+.[a-z]+@htlstp.at")]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  })
 
+  private formStatus = toSignal(this.form.valueChanges);
+
+  formError = computed(() => {
+    this.formStatus(); // trigger recompute on change
+
+    const { email, password } = this.form.controls;
+
+    if (email.touched) {
+      if (email.hasError('required'))  return 'Email is required';
+      if (email.hasError('pattern'))   return 'Use your school email (firstname.lastname@htlstp.at)';
+    }
+
+    if (password.touched) {
+      if (password.hasError('required'))    return 'Password is required';
+      if (password.hasError('minlength'))   return 'Password must be at least 8 characters';
+    }
+
+    return null;
+  });
+
+  submit() {
+    if (this.form.invalid) return;
+  }
 }
