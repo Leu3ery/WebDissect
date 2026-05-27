1d521de79427007b5fce1a2e298de5c881dd8040
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-27 12:05:22 +0200

    feat(db): add 'Endpoint' model

---
+++ backend/server/app/db/models/endpoint.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, VARCHAR, CheckConstraint
+
+from ._base import Base
+
+
+class Endpoint(Base):
+    __tablename__ = "endpoints"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    method = Column(VARCHAR(10), CheckConstraint("method IN (GET,POST,PUT,DELETE)"), nullable=False)
+    path = Column(VARCHAR(200), nullable=False)
+    status = Column(Integer, CheckConstraint("100 < status & status < 600"), nullable=False)
+    content_type = Column(VARCHAR(100), nullable=False)
+
+
