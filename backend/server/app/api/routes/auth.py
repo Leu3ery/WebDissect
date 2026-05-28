0b21ca03ddd2534d71ce0e090264fcc897ef13b8
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-28 10:03:22 +0200

    feat(schemas): add RegisterUser, LoginUser, CodeSubmit schemas

--- backend/server/app/api/routes/auth.py
+++ backend/server/app/api/routes/auth.py
@@ -1,20 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 auth = APIRouter(prefix="/auth")
 
 
+class RegisterUser(BaseModel):
+    email: str
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+class CodeSubmit(BaseModel):
+    email: str
+    code: int
+
+
 @auth.post("/register")
-def register():
+def register(register_user: RegisterUser):
     pass
 
 
 @auth.post("/login")
-def login():
+def login(login_user: LoginUser):
     pass
 
 
 @auth.post("/code/submit")
-def submit_code():
+def submit_code(code_submit: CodeSubmit):
     pass
 
 
