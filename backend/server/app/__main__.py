18484c817d1c4abf02781972bd49c80366ee3b0c
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-28 14:24:38 +0200

    fix(docker,pyproject): add fastapi sub-dependency, copy pyproject into image, change CMD to ENTRYPOINT in Dockerfiles

--- backend/server/app/__main__.py
+++ backend/server/app/__main__.py
@@ -1,7 +1,8 @@
 import os
 import uvicorn
 
-# Setup Database and start API
-# setup_db()
-# uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=os.getenv("ENV", "development") == "development")
+
+if __name__ == "__main__":
+    # setup_db()
+    uvicorn.run("app.server:app", host="0.0.0.0", port=6767, reload=os.getenv("ENV", "development") == "development")
 
