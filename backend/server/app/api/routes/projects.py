1e15122604e48fc75be42865d029739ec6c0e332
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-28 14:57:41 +0200

    feat: add db dependencies for project endpoints

--- backend/server/app/api/routes/projects.py
+++ backend/server/app/api/routes/projects.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, Depends
 from pydantic import BaseModel
+
+from app.db.db import get_db
 
 projects = APIRouter(prefix="/projects")
 
@@ -15,28 +17,28 @@ class PatchProject(BaseModel):
 
 
 @projects.get("/{project_id}")
-def get_project(project_id: int):
+def get_project(project_id: int, db = Depends(get_db)):
     pass
 
 
 @projects.post("")
-def create_project(create_project: CreateProject):
+def create_project(create_project: CreateProject, db = Depends(get_db)):
     pass
 
 
 @projects.patch("/{project_id}")
-def update_project(project_id: int, patch_project: PatchProject):
+def update_project(project_id: int, patch_project: PatchProject, db = Depends(get_db)):
     pass
 
 
 
 @projects.post("/{project_id}/upload")
-def upload_file(project_id: int, file: UploadFile):
+def upload_file(project_id: int, file: UploadFile, db = Depends(get_db)):
     pass
 
 
 
 @projects.post("/{project_id}/analysis/start")
-def start_analysis(project_id: int):
+def start_analysis(project_id: int, db = Depends(get_db)):
     pass
 
