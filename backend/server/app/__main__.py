57ec8fb05713a5f0269f499f53e3ee9dd6d6a3a8
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-28 15:08:02 +0200

    chore: initialize db on container startup

--- backend/server/app/__main__.py
+++ backend/server/app/__main__.py
@@ -1,8 +1,9 @@
 import os
 import uvicorn
 
+from app.db.db import init_db
 
 if __name__ == "__main__":
-    # setup_db()
+    init_db()
     uvicorn.run("app.server:app", host="0.0.0.0", port=6767, reload=os.getenv("ENV", "development") == "development")
 
