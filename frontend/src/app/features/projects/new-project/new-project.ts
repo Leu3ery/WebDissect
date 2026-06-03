6a42b902f0bfef2302f435600402d68d37acf3d0
Valentyn <valentun.gvozdenko.04022008@gmail.com>
2026-06-03 15:54:45 +0200

    html + css for new project

--- frontend/src/app/features/projects/new-project/new-project.ts
+++ frontend/src/app/features/projects/new-project/new-project.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {LucideKey, LucideUpload} from '@lucide/angular';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-new-project',
   imports: [
     LucideKey,
-    LucideUpload
+    LucideUpload,
+    DecimalPipe,
   ],
   templateUrl: './new-project.html',
   styleUrl: './new-project.css',
 })
 export class NewProject {
+  selectedFile = signal<null | File>(null)
 
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.item(0)
+    if (file) {
+      this.selectedFile.set(file);
+    }
+  }
+
+  getFileSize() {
+    return this.selectedFile() != null ? this.selectedFile()!.size / 1000 / 1000 : 0
+  }
+
+  getFileName() {
+    return this.selectedFile() != null ? this.selectedFile()!.name : 'no file';
+  }
 }
