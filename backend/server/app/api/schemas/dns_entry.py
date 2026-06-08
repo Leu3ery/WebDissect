729ae27afce0fb0aedb44dcfb4f6c174031c0409
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-06-08 08:45:03 +0200

    chore: update schema field descriptions

--- backend/server/app/api/schemas/dns_entry.py
+++ backend/server/app/api/schemas/dns_entry.py
@@ -1,10 +1,20 @@
 from pydantic import BaseModel, Field
+from enum import StrEnum
+
+class EntryType(StrEnum):
+    IPv4 = "A"
+    IPv6 = "AAAA"
+    MX = "MX"
+    NS = "NS"
+    TXT = "TXT"
+    # TODO
+
 
 class DNSEntry(BaseModel):
-    id: int     = Field()
-    type: str   = Field()
-    domain: str = Field()
-    value: str  = Field()
-    ttl: int    = Field()
+    id: int         = Field(description="id of DNS entry in the DB")
+    type: EntryType = Field(description="")
+    domain: str     = Field(description="")
+    value: str      = Field(description="Text value of the DNS entry")
+    ttl: int        = Field(description="Time to live of the DNS entry")
 
 
