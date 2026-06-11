d73d9d69e547b12962b7865b509324ca5ee2ef92
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-06-11 18:57:02 +0200

    fix: add domain formatting on model creation, so the netloc can be easily extracted using urlparse

--- backend/server/app/api/schemas/project.py
+++ backend/server/app/api/schemas/project.py
@@ -1,9 +1,18 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+from urllib.parse import urlparse
 
 
 class Project(BaseModel):
-    id: int | None = Field(description="Project ID")
-    name: str      = Field(description="Project Name")
-    domain: str    = Field(description="Domain of the analyzed website")
-    user_id: int   = Field(description="id of the project owner")
+    id: int | None      = Field(description="Project ID", default=None)
+    name: str           = Field(description="Project Name")
+    domain: str         = Field(description="Domain of the analyzed website")
+    user_id: int | None = Field(description="id of the project owner", default=None)
+
+    @field_validator("domain", mode="before")
+    @classmethod
+    def format_domain(cls, v: str) -> str:
+        if "//" not in v:
+            v = f"//{v}"
+        return urlparse(v).netloc
+
 
