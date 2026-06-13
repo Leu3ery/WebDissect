768e1332df930b7c7700c2a6d73c00b4d8cc07eb
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-06-13 20:30:42 +0200

    refactor: use custom class DBHandler for managing DB connections, use DBHandler.transaction() for atomic operations, register exception handlers to FastAPI instance

--- backend/server/app/__main__.py
+++ backend/server/app/__main__.py
@@ -1,9 +1,9 @@
 import os
 import uvicorn
 
-from app.db.db import init_db
+from app.db.db import db_handler
 
 if __name__ == "__main__":
-    init_db()
+    db_handler.init_db()
     uvicorn.run("app.server:app", host="0.0.0.0", port=6767, reload=os.getenv("ENV", "development") == "development")
 
--- backend/server/app/api/routes/projects.py
+++ backend/server/app/api/routes/projects.py
@@ -1,14 +1,26 @@
-from fastapi import APIRouter, UploadFile, Depends, BackgroundTasks
+from fastapi import APIRouter, UploadFile, BackgroundTasks
 
 from app.api.schemas.dns_entry import DNSEntry, EntryType
 from app.api.schemas.project import Project
-from app.db.db import get_db, session_scope
+from app.db.db import db_handler
+from app.db.models import Certificate
 from app.db.models.project import Project as DB_Project
 from app.db.models.dns_entry import DNSEntry as DB_DNSEntry
 from app.tools._dns import _query, _brute_srv, _dedupe_dns_entries
+from app.tools.tls_cert import fetch_cert, parse_cert
+
 
 projects = APIRouter(prefix="/projects")
 
+
+DNS_ENTRY_TYPES = [
+    EntryType.IPv4, EntryType.IPv6,
+    EntryType.CNAME,
+    EntryType.MX,
+    EntryType.NS,
+    EntryType.SOA,
+    EntryType.TXT
+]
 
 
 # ~~~~~~~~~~ # Utility functions # ~~~~~~~~~~ #
@@ -16,23 +28,15 @@ projects = APIRouter(prefix="/projects")
 def _fetch_dns(domain: str):
     entries: list[DNSEntry] = []
 
-    for entry_type in [
-        EntryType.IPv4, EntryType.IPv6,
-        EntryType.CNAME,
-        EntryType.MX,
-        EntryType.NS,
-        EntryType.SOA,
-        EntryType.TXT
-    ]:
+    for entry_type in DNS_ENTRY_TYPES:
         entries.extend(_query(domain, entry_type))
 
     entries.extend(_brute_srv(domain))
     entries = _dedupe_dns_entries(entries)
 
-    with session_scope() as db:
+    with db_handler.transaction() as db:
         # Delete current DB entries, insert new Records
         db.query(DB_DNSEntry).delete(synchronize_session=False)
-        db.commit()
 
         for entry in entries:
             db.add(DB_DNSEntry(
@@ -41,48 +45,57 @@ def _fetch_dns(domain: str):
                 value=entry.value,
                 ttl=entry.ttl
             ))
-        db.commit()
+
+
+def _fetch_cert(domain: str):
+    # TODO handle exceptions
+    raw_cert = fetch_cert(domain)
+    cert = parse_cert(raw_cert)
+
+    with db_handler.transaction() as db:
+        # Delete last certificate and replace with current one
+        db.query(Certificate).delete(synchronize_session=False)
+        db.add(cert)
+
 
 
 
 @projects.get("/{project_id}")
-def get_project(project_id: int, db = Depends(get_db)):
+def get_project(project_id: int):
     pass
 
 
 @projects.post("")
 def create_project(
     create_project: Project,
-    bg: BackgroundTasks,
-    db = Depends(get_db),
+    bg: BackgroundTasks
 ):
-    # TODO: catch pydantic ValidationError
-    db.add(DB_Project(
-        name=create_project.name,
-        domain=create_project.domain,
-        # user_id=user.id
-        user_id=1   # only for testing
-    ))
-    db.commit()
+    with db_handler.transaction() as db:
+        db.add(DB_Project(
+            name=create_project.name,
+            domain=create_project.domain,
+            # user_id=user.id
+            user_id=1   # only for testing
+        ))
 
     bg.add_task(_fetch_dns, create_project.domain)
-    # bg.add_task(_fetch_certs, create_project.domain)
-    return {"success" : True}
+    bg.add_task(_fetch_cert, create_project.domain)
+    return {"success" : True}  # only for testing
 
 
 @projects.patch("/{project_id}")
-def update_project(project_id: int, patch_project: Project, db = Depends(get_db)):
+def update_project(project_id: int, patch_project: Project):
     pass
 
 
 
 @projects.post("/{project_id}/upload")
-def upload_file(project_id: int, file: UploadFile, db = Depends(get_db)):
+def upload_file(project_id: int, file: UploadFile):
     pass
 
 
 
 @projects.post("/{project_id}/analysis/start")
-def start_analysis(project_id: int, db = Depends(get_db)):
+def start_analysis(project_id: int):
     pass
 
