8694fb510f20365425cf5dfad39a6c9750dc3598
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-06-13 22:34:30 +0200

    style: indents

--- backend/server/app/api/schemas/dns_entry.py
+++ backend/server/app/api/schemas/dns_entry.py
@@ -13,10 +13,10 @@ class DNSEntryType(StrEnum):
 
 
 class DNSEntry(BaseModel):
-    id: int | None  = Field(description="id of DNS entry in the DB", default=None)
+    id: int | None     = Field(description="id of DNS entry in the DB", default=None)
     type: DNSEntryType = Field(description="")
-    domain: str     = Field(description="")
-    value: str      = Field(description="Text value of the DNS entry")
-    ttl: int        = Field(description="Time to live of the DNS entry")
+    domain: str        = Field(description="")
+    value: str         = Field(description="Text value of the DNS entry")
+    ttl: int           = Field(description="Time to live of the DNS entry")
 
 
