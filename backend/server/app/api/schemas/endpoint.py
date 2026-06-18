5f5687fbbab9d74a2857cbd2c3479a9e0254084d
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-06-18 21:36:01 +0200

    feat: finish har analysis

--- backend/server/app/api/schemas/endpoint.py
+++ backend/server/app/api/schemas/endpoint.py
@@ -1,13 +1,29 @@
-from pydantic import BaseModel, Field
+from enum import StrEnum
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class HTTPMethod(StrEnum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    PATCH = 'PATCH'
+    DELETE = 'DELETE'
+    HEAD = 'HEAD'
+    OPTIONS = 'OPTIONS'
+
 
 class Endpoint(BaseModel):
     """
     Representation of a single HTTP endpoint (URL + HTTP Method)
     """
-    id: int | None    = Field(description="id of the endpoint in the DB")
-    method: str       = Field(description="HTTP Method accepted by the endpoint")
-    path: str         = Field(description="Path of the endpoint")
-    status: int       = Field(description="HTTP Status code of the response")
-    content_type: str = Field(description="Response content type")
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int | None     = Field(description="id of the endpoint in the DB", default=None)
+    analysis_id: int   = Field()
+    method: HTTPMethod = Field(description="HTTP Method accepted by the endpoint")
+    path: str          = Field(description="Path of the endpoint")
+    status: int        = Field(description="HTTP Status code of the response")
+    content_type: str  = Field(description="Response content type")
+
 
 
--- backend/server/app/db/models/endpoint.py
+++ backend/server/app/db/models/endpoint.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR, CheckConstraint
+from sqlalchemy import Column, Integer, VARCHAR, CheckConstraint, ForeignKey
 
 from ._base import Base
 
@@ -7,9 +7,10 @@ class Endpoint(Base):
     __tablename__ = "endpoints"
 
     id           = Column(Integer, primary_key=True, autoincrement=True)
-    method       = Column(VARCHAR(10), CheckConstraint("method IN ('GET','POST','PUT','DELETE')"), nullable=False)
+    analysis_id  = Column(Integer, ForeignKey("analyses.id"))
+    method       = Column(VARCHAR(10), CheckConstraint("method IN ('GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS')"), nullable=False)
     path         = Column(VARCHAR(200), nullable=False)
-    status       = Column(Integer, CheckConstraint("100 < status & status < 600"), nullable=False)
+    status       = Column(Integer, nullable=False)
     content_type = Column(VARCHAR(100), nullable=False)
 
 
